c39d2e926787e9c7a5eac00edf758c0150292426
plll0123 <jsh951227@gmail.com>
2023-11-14 00:47:12 +0900

    test 1

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:17-jdk
+
+ARG NAMEZ
+
+ENV NAME=${NAMEZ}
--- src/main/java/com/example/demo/FooController.java
+++ src/main/java/com/example/demo/FooController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +10,10 @@ public class FooController {
 	@GetMapping("/foo")
 	String foo() {
 		return "foo";
+	}
+
+	@GetMapping("/var")
+	String var(@Value("${name}") String name) {
+		return name;
 	}
 }
